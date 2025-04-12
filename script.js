6af051138f8d4a26a70c2346678af7a4f5f09ef7
Rafay <rms7@williams.edu>
2025-04-12 18:00:54 -0400

    Added hovering option

--- script.js
+++ script.js
@@ -1,22 +1,80 @@
 //*** TO DO: Add a 'genre' tag for each book title
 const books = [
   //Nonfiction titles
-  { title: "Pride and Prejudice", author: "Jane Austen", genre: "Nonfiction", imageLink: "https://m.media-amazon.com/images/I/712P0p5cXIL._AC_UF1000,1000_QL80_.jpg"},
-  { title: "A Short History of Nearly Everything", author: "Bill Bryson", genre: "Nonfiction", imageLink: "https://upload.wikimedia.org/wikipedia/en/e/ed/Bill_bryson_a_short_history.jpg"},
-  { title: "The Diary of a Young Girl", author: "Anne Frank", genre: "Nonfiction", imageLink: "https://images.booksense.com/images/097/643/9781417643097.jpg"},
-  { title: "Outliers: The Story of Success", author: "Malcolm Gladwell", genre: "Nonfiction", imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344266315i/3228917.jpg"},
-  { title: "Night", author: "Elie Wiesel", genre: "Nonfiction", imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1709507006i/1617.jpg"},
-  { title: "The Immortal Life of Henrietta Lacks", author: "Rebecca Skloot", genre: "Nonfiction", imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1327878144i/6493208.jpg"},
-  { title: "Blink: The Power of Thinking Without Thinking", author: "Malcolm Gladwell", genre: "Nonfiction", imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1440763417i/40102.jpg"},
-  { title: "Midnight in the Garden of Good and Evil", author: "John Berendt", genre: "Nonfiction", imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1427166915i/386187.jpg"},
-  { title: "Sapiens: A Brief History of Humankind", author: "Yuval Noah Harari", genre: "Nonfiction", imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1703329310i/23692271.jpg"},
-  { title: "The God Delusion", author: "Richard Dawkins", genre: "Nonfiction", imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347220693i/14743.jpg"},
+  { title: "Pride and Prejudice", 
+    author: "Jane Austen", 
+    genre: "Nonfiction", 
+    summary: "A witty and romantic novel following Elizabeth Bennet as she navigates love, class, and misunderstandings in 19th-century England, particularly with the proud yet intriguing Mr. Darcy.",
+    imageLink: "https://m.media-amazon.com/images/I/712P0p5cXIL._AC_UF1000,1000_QL80_.jpg"
+  },
+  { title: "A Short History of Nearly Everything", 
+    author: "Bill Bryson", 
+    genre: "Nonfiction", 
+    summary: "A humorous and engaging exploration of science and the universe, where Bryson explains everything from the Big Bang to the rise of humans, making complex topics accessible and entertaining.",
+    imageLink: "https://upload.wikimedia.org/wikipedia/en/e/ed/Bill_bryson_a_short_history.jpg"
+  },
+  { 
+    title: "The Diary of a Young Girl", 
+    author: "Anne Frank", 
+    genre: "Nonfiction", 
+    imageLink: "https://images.booksense.com/images/097/643/9781417643097.jpg"
+  },
+  { title: "Outliers: The Story of Success", 
+    author: "Malcolm Gladwell", 
+    genre: "Nonfiction", 
+    imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344266315i/3228917.jpg"
+  },
+  { title: "Night", 
+    author: "Elie Wiesel", 
+    genre: "Nonfiction", 
+    imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1709507006i/1617.jpg"
+  },
+  { 
+    title: "The Immortal Life of Henrietta Lacks", 
+    author: "Rebecca Skloot", 
+    genre: "Nonfiction", 
+    imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1327878144i/6493208.jpg"
+  },
+  { title: "Blink: The Power of Thinking Without Thinking", 
+    author: "Malcolm Gladwell",
+    genre: "Nonfiction", 
+    imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1440763417i/40102.jpg"
+  },
+  { title: "Midnight in the Garden of Good and Evil", 
+    author: "John Berendt", genre: "Nonfiction", 
+    imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1427166915i/386187.jpg"
+  },
+  { title: "Sapiens: A Brief History of Humankind", 
+    author: "Yuval Noah Harari", 
+    genre: "Nonfiction", 
+    imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1703329310i/23692271.jpg"
+  },
+  { title: "The God Delusion", 
+    author: "Richard Dawkins", 
+    genre: "Nonfiction", 
+    imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347220693i/14743.jpg"
+  },
 
   //Fiction Titles
-  { title: "Adventures of Huckleberry Finn", author: "Mark Twain", genre: "Fiction", imageLink: "https://www.ucpress.edu/_next/image?url=https%3A%2F%2Fwebfiles.ucpress.edu%2Fcoverimage%2Fisbn13%2F9780520343641.jpg&w=1920&q=90"},
-  { title: "The God of the Woods", author: "Liz Moore", genre: "Fiction", imageLink: "https://m.media-amazon.com/images/I/81gHfeKi+9L.jpg"},
-  { title: "Fourth Wing", author: "Rebecca Yarros", genre: "Fiction", imageLink: "https://m.media-amazon.com/images/I/71bXcusLgJL.jpg"},
-  { title: "The God of the Woods", author: "Liz Moore", genre: "Fiction", imageLink: ""}
+  { title: "Adventures of Huckleberry Finn", 
+    author: "Mark Twain", 
+    genre: "Fiction", 
+    imageLink: "https://www.ucpress.edu/_next/image?url=https%3A%2F%2Fwebfiles.ucpress.edu%2Fcoverimage%2Fisbn13%2F9780520343641.jpg&w=1920&q=90"
+  },
+  { title: "The God of the Woods", 
+    author: "Liz Moore", 
+    genre: "Fiction", 
+    imageLink: "https://m.media-amazon.com/images/I/81gHfeKi+9L.jpg"
+  },
+  { title: "Fourth Wing", 
+    author: "Rebecca Yarros", 
+    genre: "Fiction", 
+    imageLink: "https://m.media-amazon.com/images/I/71bXcusLgJL.jpg"
+  },
+  { title: "The God of the Woods", 
+    author: "Liz Moore", 
+    genre: "Fiction", imageLink: ""
+  }
   
 
   //Horror Titles
@@ -44,7 +102,7 @@ function renderBooks(bookList) {
 
     //Adds the proper heading to the div
     const genreHeading = document.createElement("h3");
-    genreHeading.textContent = `${genre} Titles:`;
+    genreHeading.textContent = `${genre}`;
     genreContainer.appendChild(genreHeading);
 
     //Add the genre container to the main container
@@ -80,10 +138,26 @@ function renderBooks(bookList) {
     authorEl.className = "card-text";
     authorEl.textContent = `by ${book.author}`;
 
+    //overlay for the summary
+    const overlay = document.createElement("div");
+    overlay.className = "book-overlay";
+
+    //overlay content (the summary)
+    const overlayContent = document.createElement("div");
+
+    //summary 
+    const summary = document.createElement("summary");
+    summary.textContent = book.summary;
+
+    //adds summary to overlayContent
+    overlayContent.append(summary);
+    overlay.appendChild(overlayContent);
+
     //Adds the image, title, and author of the book to the card
     card.appendChild(imgEl);
     card.appendChild(titleEl);
     card.appendChild(authorEl);
+    card.appendChild(overlay);
 
     col.appendChild(card);
 
@@ -92,6 +166,7 @@ function renderBooks(bookList) {
     genreContainer.appendChild(col);
   });
 }
+
 
 
 // Filter books by search input
