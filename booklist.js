11ae5849cdbbf017e6bc976d20e2aaeae07b8907
26gdr2 <gdr2@williams.edu>
2025-04-12 20:29:21 -0400

    Added all titles; divded book data and javascript functions that manipulate DOM into seperate files

---
+++ booklist.js
@@ -0,0 +1,309 @@
+export const books = [
+    //Nonfiction titles
+    { title: "Pride and Prejudice", 
+      author: "Jane Austen", 
+      genre: "Nonfiction", 
+      summary: "A witty and romantic novel following Elizabeth Bennet as she navigates love, class, and misunderstandings in 19th-century England, particularly with the proud yet intriguing Mr. Darcy.",
+      imageLink: "https://m.media-amazon.com/images/I/712P0p5cXIL._AC_UF1000,1000_QL80_.jpg"
+    },
+    { title: "A Short History of Nearly Everything", 
+      author: "Bill Bryson", 
+      genre: "Nonfiction", 
+      summary: "A humorous and engaging exploration of science and the universe, where Bryson explains everything from the Big Bang to the rise of humans, making complex topics accessible and entertaining.",
+      imageLink: "https://upload.wikimedia.org/wikipedia/en/e/ed/Bill_bryson_a_short_history.jpg"
+    },
+    { 
+      title: "The Diary of a Young Girl", 
+      author: "Anne Frank", 
+      genre: "Nonfiction", 
+      imageLink: "https://images.booksense.com/images/097/643/9781417643097.jpg"
+    },
+    { title: "Outliers: The Story of Success", 
+      author: "Malcolm Gladwell", 
+      genre: "Nonfiction", 
+      imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344266315i/3228917.jpg"
+    },
+    { title: "Night", 
+      author: "Elie Wiesel", 
+      genre: "Nonfiction", 
+      imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1709507006i/1617.jpg"
+    },
+    { 
+      title: "The Immortal Life of Henrietta Lacks", 
+      author: "Rebecca Skloot", 
+      genre: "Nonfiction", 
+      imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1327878144i/6493208.jpg"
+    },
+    { title: "Blink: The Power of Thinking Without Thinking", 
+      author: "Malcolm Gladwell",
+      genre: "Nonfiction", 
+      imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1440763417i/40102.jpg"
+    },
+    { title: "Midnight in the Garden of Good and Evil", 
+      author: "John Berendt", genre: "Nonfiction", 
+      imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1427166915i/386187.jpg"
+    },
+    { title: "Sapiens: A Brief History of Humankind", 
+      author: "Yuval Noah Harari", 
+      genre: "Nonfiction", 
+      imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1703329310i/23692271.jpg"
+    },
+    { title: "The God Delusion", 
+      author: "Richard Dawkins", 
+      genre: "Nonfiction", 
+      imageLink: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347220693i/14743.jpg"
+    },
+  
+    //Fiction Titles
+    { title: "Adventures of Huckleberry Finn", 
+      author: "Mark Twain", 
+      genre: "Fiction", 
+      imageLink: "https://www.ucpress.edu/_next/image?url=https%3A%2F%2Fwebfiles.ucpress.edu%2Fcoverimage%2Fisbn13%2F9780520343641.jpg&w=1920&q=90"
+    },
+    { title: "The God of the Woods", 
+      author: "Liz Moore", 
+      genre: "Fiction", 
+      imageLink: "https://m.media-amazon.com/images/I/81gHfeKi+9L.jpg"
+    },
+    { title: "Fourth Wing", 
+      author: "Rebecca Yarros", 
+      genre: "Fiction", 
+      imageLink: "https://m.media-amazon.com/images/I/71bXcusLgJL.jpg"
+    },
+    { title: "The God of the Woods", 
+      author: "Liz Moore", 
+      genre: "Fiction", 
+      imageLink: "https://m.media-amazon.com/images/I/81gHfeKi+9L.jpg"
+    },
+    {
+        title: "Project Hail Mary",
+        author: "Andy Weir",
+        genre: "Fiction",
+        imageLink: "https://m.media-amazon.com/images/I/81zD9kaVW9L.jpg"
+    },
+    {
+        title: "Remarkably Bright Creatures",
+        author: "Shelby Van Pelt",
+        genre: "Fiction",
+        imageLink: "https://m.media-amazon.com/images/I/81X7rAcaQkL._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+        title: "The Nightingale",
+        author: "Kristin Hannah",
+        genre: "Fiction",
+        imageLink: "https://m.media-amazon.com/images/I/81OkWjcf4WL._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+        title: "People We Meet on Vacation",
+        author: "Emily Henry",
+        genre: "Fiction",
+        imageLink: "https://m.media-amazon.com/images/I/71WsX3abgiL.jpg"
+    },
+    {
+        title: "Daisy Jones and The Six",
+        author: "Taylor Jenkins Reid",
+        genre: "Fiction",
+        imageLink: "https://m.media-amazon.com/images/I/81cxxjQd57L.jpg"
+    },
+    {
+        title: "Small Things Like These",
+        author: "Claire Keegan",
+        genre: "Fiction",
+        imageLink: "https://m.media-amazon.com/images/I/71F6+OyiN7L.jpg"
+    },
+    {
+        title: "Ready Player Two",
+        author: "Ernest Cline",
+        genre: "Fiction",
+        imageLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRT9TI6dxNX-L0th_EFXxS8EjB6W6CraWg4LQ&s"
+    },
+    
+    //Horror Titles
+    {
+        title: "The Only Good Indians",
+        author: "Stephen Graham Jones",
+        genre: "Horror",
+        imageLink: "https://m.media-amazon.com/images/I/716rRntzCyL.jpg"
+    },
+    {
+        title: "Home Before Dark",
+        author: "Todd Ritter",
+        genre: "Horror",
+        imageLink: "https://m.media-amazon.com/images/I/81JqiYyapjL._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+        title: "A Head Full of Ghosts",
+        author: "Paul G. Tremblay",
+        genre: "Horror",
+        imageLink: "https://m.media-amazon.com/images/I/81JDDs3NEXL.jpg"
+    },
+    {
+        title: "The Only One Left",
+        author: "Todd Ritter",
+        genre: "Horror",
+        imageLink: "https://m.media-amazon.com/images/I/81pvjWH+4HL._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+        title: "The Final Girl Support Group",
+        author: "Grady Hendrix",
+        genre: "Horror",
+        imageLink: "https://m.media-amazon.com/images/I/71-z+-TmvnL.jpg"
+    },
+    {
+        title: "The Ruins",
+        author: "Scott Smith",
+        genre: "Horror",
+        imageLink: "https://m.media-amazon.com/images/I/81oNmUkGJuL._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+        title: "Fairy Tale",
+        author: "Stephen King",
+        genre: "Horror",
+        imageLink: "https://m.media-amazon.com/images/I/81blQfKsLtL.jpg"
+    },
+    {
+        title: "Devolution",
+        author: "Max Brooks",
+        genre: "Horror",
+        imageLink: "https://m.media-amazon.com/images/I/91pzoeF-oCL.jpg"
+    },
+    {
+        title: "The Institute",
+        author: "Stephen King",
+        genre: "Horror",
+        imageLink: "https://m.media-amazon.com/images/I/71s6siGLrFL.jpg"
+    },
+    {
+        title: "House of Leaves",
+        author: "Mark Z. Danielewski",
+        genre: "Horror",
+        imageLink: "https://m.media-amazon.com/images/I/61yArGR8YkL.jpg"
+    },
+  
+    //Mystery Titles
+    {
+        title: "We Solve Murders",
+        author: "Richard Osman",
+        genre: "Mystery",
+        imageLink: "https://m.media-amazon.com/images/I/71C-g7glLoL.jpg"
+    },
+    {
+        title: "Gone Girl",
+        author: "Gillian Flynn",
+        genre: "Mystery",
+        imageLink: "https://m.media-amazon.com/images/I/713e4Yk6brL.jpg"
+    },
+    {
+        title: "Nobody's Fool",
+        author: "Harlan Coben",
+        genre: "Mystery",
+        imageLink: "https://m.media-amazon.com/images/I/81z9HmIYbkL.jpg"
+    },
+    {
+        title: "Where the Crawdads Sing",
+        author: "Delia Owens",
+        genre: "Mystery",
+        imageLink: "https://m.media-amazon.com/images/I/81F0NTrPdCL._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+        title: "The Guest List",
+        author: "Lucy Foley",
+        genre: "Mystery",
+        imageLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr5Wx5loO19jiUGeBOocprvM63-HGxS1V4UA&s"
+    },
+    {
+        title: "Murder Your Employer",
+        author: "Rupert Holmes",
+        genre: "Mystery",
+        imageLink: "https://m.media-amazon.com/images/I/81N8mz37+QL._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+        title: "Magpie Murders",
+        author: "Anthony Horowitz",
+        genre: "Mystery",
+        imageLink: "https://m.media-amazon.com/images/I/71VksxQMBUL.jpg"
+    },
+    {
+        title: "All the Sinners Bleed",
+        author: "S. A. Cosby",
+        genre: "Mystery",
+        imageLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPjuuvs1dRnlSorSZKn9KQO1biVkp_GN55qw&s"
+    },
+    {
+        title: "I Will Find You",
+        author: "Harlan Coben",
+        genre: "Mystery",
+        imageLink: "https://m.media-amazon.com/images/I/81BgHPMdk9L.jpg"
+    },
+    {
+        title: "Hidden Pictures",
+        author: "Jason Rekulak",
+        genre: "Mystery",
+        imageLink: "https://m.media-amazon.com/images/I/91puw8POkwL.jpg"
+    },
+    
+    //Romance Titles
+    {
+        title: "Yours Truly",
+        author: "Abby Jimenez",
+        genre: "Romance",
+        imageLink: "https://m.media-amazon.com/images/I/71oBxCBe2yL.jpg"
+    },
+    {
+        title: "Hooked",
+        author: "Emily McIntire",
+        genre: "Romance",
+        imageLink: "https://m.media-amazon.com/images/I/71Kmqii-QvL._UF1000,1000_QL80_.jpg"
+    },
+    {
+        title: "The Dead Romantics",
+        author: "Ashley Poston",
+        genre: "Romance",
+        imageLink: "https://m.media-amazon.com/images/I/81MT8btQrsL.jpg"
+    },
+    {
+        title: "Just for the Summer",
+        author: "Abby Jimenez",
+        genre: "Romance",
+        imageLink: "https://m.media-amazon.com/images/I/81tVBUv75vL.jpg"
+    },
+    {
+        title: "Onyx Storm",
+        author: "Rebecca Yarros",
+        genre: "Romance",
+        imageLink: "https://m.media-amazon.com/images/I/91B2LWGhxSL._AC_UF1000,1000_QL80_.jpg"
+    },
+    {
+        title: "Fifty Shades Freed",
+        author: "E. L. James",
+        genre: "Romance",
+        imageLink: "https://m.media-amazon.com/images/I/81x8fk0sRLL.jpg"
+    },
+    {
+        title: "God of Fury",
+        author: "Rina Kent",
+        genre: "Romance",
+        imageLink: "https://m.media-amazon.com/images/I/71ceBXa6W2L._AC_UF1000,1000_QL80_DpWeblab_.jpg"
+    },
+    {
+        title: "Before I Let Go",
+        author: "Kennedy Ryan",
+        genre: "Romance",
+        imageLink: "https://m.media-amazon.com/images/I/91BxrwsBrKL.jpg"
+    },
+    {
+        title: "The Lost Bookshop",
+        author: "Evie Woods",
+        genre: "Romance",
+        imageLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFtS5Dcu-Ccxq4MJGcE4xQRvSYLlkMGmzpSw&s"
+    },
+    {
+        title: "Flirting Lessons",
+        author: "Jasmine Guillory",
+        genre: "Romance",
+        imageLink: "https://m.media-amazon.com/images/I/81h1DqFYltL._AC_UF1000,1000_QL80_.jpg"
+    },
+    
+  ];
+
+export const fakeSummary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
